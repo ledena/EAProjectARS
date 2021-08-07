09d6ab22f9367a356c85c32ba45f76a21e5ce8f1
Eden <asnakeeden@gmail.com>
2021-08-07 11:32:29 -0500

    'Added'

---
+++ src/main/java/com/edu/cs554/airlinesreservationsystem/AirlinesReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.edu.cs554.airlinesreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlinesReservationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/cs554/airlinesreservationsystem/AirlinesReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cs554.airlinesreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
