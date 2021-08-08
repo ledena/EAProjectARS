679d945e53c02b69b1815be0c4199f56443bf558
Eden <asnakeeden@gmail.com>
2021-08-07 20:31:42 -0500

    Airlines class added Flight class added

--- src/main/java/com/edu/cs554/airlinesreservationsystem/model/Airlines.java
+++ src/main/java/com/edu/cs554/airlinesreservationsystem/model/Airlines.java
@@ -1,4 +1,31 @@
 package com.edu.cs554.airlinesreservationsystem.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@SecondaryTable(name="airline_history",
+        pkJoinColumns=@PrimaryKeyJoinColumn(name="history"))
 public class Airlines {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String code;
+    private String name;
+
+
+    private String history;
+
+
+
 }
--- src/main/java/com/edu/cs554/airlinesreservationsystem/model/Flight.java
+++ src/main/java/com/edu/cs554/airlinesreservationsystem/model/Flight.java
@@ -1,4 +1,28 @@
 package com.edu.cs554.airlinesreservationsystem.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+
+@Setter
+@Getter
+@Entity
 public class Flight {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private  int capacity;
+    private String number;
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+
 }
