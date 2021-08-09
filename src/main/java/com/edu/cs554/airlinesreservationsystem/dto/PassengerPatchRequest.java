64cb81b4321d22cbd13ec16f748e604ee62f5a46
abdibedaso <atbedaso@gmail.com>
2021-08-08 22:25:51 -0500

    Create Request Dto's

---
+++ src/main/java/com/edu/cs554/airlinesreservationsystem/dto/PassengerPatchRequest.java
@@ -0,0 +1,23 @@
+package com.edu.cs554.airlinesreservationsystem.dto;
+
+import com.edu.cs554.airlinesreservationsystem.model.Address;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class PassengerPatchRequest {
+    private final long passengerId;
+    private final String firstName;
+    private final String lastName;
+    @JsonFormat(pattern = "YYYY-MM-dd")
+    private final LocalDate dateOfBirth;
+}
