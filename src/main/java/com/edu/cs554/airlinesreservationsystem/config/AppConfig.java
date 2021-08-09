c7a1ecddbefee28cabdd9d52f80acd426b9ab935
abdibedaso <atbedaso@gmail.com>
2021-08-08 23:09:53 -0500

    Create ModelMapper Bean

--- src/main/java/com/edu/cs554/airlinesreservationsystem/config/AppConfig.java
+++ src/main/java/com/edu/cs554/airlinesreservationsystem/config/AppConfig.java
@@ -1,8 +1,16 @@
 package com.edu.cs554.airlinesreservationsystem.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper;
+    }
 
 }
