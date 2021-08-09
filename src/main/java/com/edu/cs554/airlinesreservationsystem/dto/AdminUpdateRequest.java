5238808dcfc85e6528a51a54539672a34316876a
abdibedaso <atbedaso@gmail.com>
2021-08-09 09:32:55 -0500

    Create Admin request dto's

---
+++ src/main/java/com/edu/cs554/airlinesreservationsystem/dto/AdminUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.edu.cs554.airlinesreservationsystem.dto;
+
+import com.edu.cs554.airlinesreservationsystem.model.Address;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class AdminUpdateRequest {
+    private final String firstName;
+    private final String lastName;
+    private final Address residenceAddress;
+}
