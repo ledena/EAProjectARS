507a000b618801c7472adfff71e927fd2ef11c18
abdibedaso <atbedaso@gmail.com>
2021-08-08 22:23:17 -0500

    Add UserRepository.java

--- src/main/java/com/edu/cs554/airlinesreservationsystem/repositories/UserRepository.java
+++ src/main/java/com/edu/cs554/airlinesreservationsystem/repositories/UserRepository.java
@@ -3,9 +3,12 @@ package com.edu.cs554.airlinesreservationsystem.repositories;
 import com.edu.cs554.airlinesreservationsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional(propagation = Propagation.REQUIRED)
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUserName(String username);
+    User findByUsername(String username);
     User findById(int id);
 }
