29a8ef1cbeb37f986e35e41d08191dce819da67d
abdibedaso <atbedaso@gmail.com>
2021-08-08 22:16:45 -0500

    Remove Ms Sql configs

--- src/main/java/com/edu/cs554/airlinesreservationsystem/AirlinesReservationSystemApplication.java
+++ src/main/java/com/edu/cs554/airlinesreservationsystem/AirlinesReservationSystemApplication.java
@@ -2,7 +2,6 @@ package com.edu.cs554.airlinesreservationsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class AirlinesReservationSystemApplication {
--- src/main/java/com/edu/cs554/airlinesreservationsystem/config/AppConfig.java
+++ src/main/java/com/edu/cs554/airlinesreservationsystem/config/AppConfig.java
@@ -1,15 +1,8 @@
 package com.edu.cs554.airlinesreservationsystem.config;
 
-import edu.miu.common.config.CommonServiceConfig;
-import org.springframework.context.annotation.Bean;
-import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Configuration;
 
-public class AppConfig extends CommonServiceConfig {
-
-    @Bean
-    public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-        return modelMapper;
-    }
+@Configuration
+public class AppConfig {
 
 }
