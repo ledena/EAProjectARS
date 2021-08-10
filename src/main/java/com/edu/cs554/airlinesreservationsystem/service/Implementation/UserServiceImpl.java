35d66c0b36ab68073cf62088acaa1051facb988a
abdibedaso <atbedaso@gmail.com>
2021-08-10 10:52:24 -0500

    Fetch granted authority in UserServiceImpl

--- src/main/java/com/edu/cs554/airlinesreservationsystem/service/Implementation/UserServiceImpl.java
+++ src/main/java/com/edu/cs554/airlinesreservationsystem/service/Implementation/UserServiceImpl.java
@@ -5,14 +5,15 @@ import com.edu.cs554.airlinesreservationsystem.repository.UserRepository;
 import com.edu.cs554.airlinesreservationsystem.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -28,7 +29,13 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(userName);
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getAuthority(user));
+    }
+
+    private Set getAuthority(User user) {
+        Set authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+        return authorities;
     }
 
     public User encodePassword(User user){
