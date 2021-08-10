c97e48db46b62666403af9ffe80bc40d0f8e9d2b
abdibedaso <atbedaso@gmail.com>
2021-08-10 10:59:41 -0500

    Authorize user controller

--- src/main/java/com/edu/cs554/airlinesreservationsystem/controller/UserController.java
+++ src/main/java/com/edu/cs554/airlinesreservationsystem/controller/UserController.java
@@ -9,9 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@PreAuthorize("permitAll()")
 @RestController
 @RequestMapping(path="/user")
 public class UserController{
